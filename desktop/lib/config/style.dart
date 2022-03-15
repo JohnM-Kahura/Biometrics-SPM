d2717ff8fdb3ec804168acb3fbf4b645635dd800
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-15 12:14:03 +0300

    global styles for app

---
+++ desktop/lib/config/style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+TextStyle sideMenuText=  const TextStyle(
+  color:Colors.white,
+  fontWeight: FontWeight.bold,
+
+);
+TextStyle bottomTabLeading= const TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+
+);
+TextStyle bottomTabUserInfo= const TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+
+);
+
+
