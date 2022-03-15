acc7051b3ad545bfa1b6d18869676728366f99af
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-15 13:27:26 +0300

    flutter desktop pac added

--- desktop/lib/main.dart
+++ desktop/lib/main.dart
@@ -2,11 +2,12 @@ import 'dart:io';
 import 'package:desktop/container/container.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:desktop_window/desktop_window.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   if(!kIsWeb&& Platform.isMacOS||Platform.isWindows||Platform.isLinux){
-//  await  DesktopWindow.setMinWindowSize(const Size(600,800));
+ await  DesktopWindow.setMinWindowSize(const Size(600,800));
 
   }
   runApp(const MyApp());
