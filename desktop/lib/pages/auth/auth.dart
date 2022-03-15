7a68b477688056791f6629fe890f5c6d95714cca
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-15 12:14:38 +0300

    auth pages

---
+++ desktop/lib/pages/auth/auth.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Auth extends StatefulWidget {
+  const Auth({ Key? key }) : super(key: key);
+
+  @override
+  State<Auth> createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
