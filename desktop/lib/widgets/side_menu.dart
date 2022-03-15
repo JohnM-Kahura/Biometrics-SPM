c8f18f7a85c03e7ae7ac9c7a35e16dd37a6751d2
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-15 14:24:01 +0300

    added logo

--- desktop/lib/widgets/side_menu.dart
+++ desktop/lib/widgets/side_menu.dart
@@ -8,23 +8,26 @@ class SideMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(10),
-      width: 280,
-      color: Color.fromARGB(255, 26, 24, 24),
+      width: 250,
+      color: const Color.fromARGB(255, 26, 24, 24),
       height: double.infinity,
       child: Column(
         children:   [
-          // Row(
-          //   children: [
-          //     Padding(
-          //       padding: const EdgeInsets.all(16.0),
-          //       child: Image.asset(
-          //         'assets/team_logo.png',
-          //         height: 60,
-          //         filterQuality: FilterQuality.high,
-          //       ),
-          //     ),
-          //   ],
-          // ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset(
+                  
+                  'assets/team_logo.png',
+                  height: 60,
+                  width: 180,
+                  color: Colors.white,
+                  filterQuality: FilterQuality.high,
+                ),
+              ),
+            ],
+          ),
           ListTile(leading: const Icon(Icons.home,color: Colors.white,),title: Text('Register users',style: sideMenuText,overflow: TextOverflow.ellipsis,),),
           ListTile(leading: const Icon(Icons.verified,color: Colors.white,),title: Text('Verify users',style: sideMenuText,overflow: TextOverflow.ellipsis,),),
           // ListTile(leading: const Icon(Icons.person),title: Text('Account',style: TextStyle(color: Colors.white,),overflow: TextOverflow.ellipsis,),),
