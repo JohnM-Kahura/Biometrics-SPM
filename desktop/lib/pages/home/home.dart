c1a262981019ea47df707024f8602df2210667b2
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-16 19:47:42 +0300

    re-design of home page

--- desktop/lib/pages/home/home.dart
+++ desktop/lib/pages/home/home.dart
@@ -10,33 +10,49 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   TextEditingController firstnameController = TextEditingController();
+  TextEditingController middlenameController = TextEditingController();
   TextEditingController lastnameController = TextEditingController();
   TextEditingController studentIDController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 235, 227, 227),
+      backgroundColor: const Color.fromARGB(255, 235, 227, 227),
       appBar: AppBar(
         elevation: 0,
+        title: const Center(child: Text('User Registration')),
       ),
       body: Row(children: [
-        Container(
-          // color: Colors.red,
+        SizedBox(
           height: double.infinity,
           width: MediaQuery.of(context).size.width / 3.2,
           child: Column(
             children: [
               customTextFeild(50, double.infinity, 'First name',
-                  firstnameController, TextInputType.name),
+                  firstnameController, TextInputType.name,false),
+              customTextFeild(50, double.infinity, 'Middle name',
+                  middlenameController, TextInputType.name,false),
               customTextFeild(50, double.infinity, 'Last name',
-                  lastnameController, TextInputType.name),
+                  lastnameController, TextInputType.name,false),
               customTextFeild(50, double.infinity, 'StudentID',
-                  studentIDController, TextInputType.name),
+                  studentIDController, TextInputType.name,false),
+              Container(
+                margin: const EdgeInsets.only(top: 30),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
-        Container(
-          // color: Colors.amber,
+        SizedBox(
           height: double.infinity,
           width: MediaQuery.of(context).size.width / 2.5,
           child: Column(
@@ -46,20 +62,31 @@ class _HomeState extends State<Home> {
                 color: Colors.grey,
                 width: 250,
                 height: 250,
-                child: const  Icon(
+                child: const Icon(
                   Icons.fingerprint,
                   size: 150,
                   color: Colors.green,
                 ),
               ),
               Container(
-                width: 250,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.white),
-                child: Column(children: [
-                Row(children: [Text('Status:'),Text('Sucess')],),
-                Row(children: [Text('Output:'),Text('Finger print scan done')],),
-              ],)),
+                  width: 250,
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: const  [ Text('Status:'), Text('Sucess')],
+                      ),
+                      Row(
+                        children: const [
+                          Text('Output:'),
+                          Text('Finger print scan done')
+                        ],
+                      ),
+                    ],
+                  )),
             ],
           ),
         )
