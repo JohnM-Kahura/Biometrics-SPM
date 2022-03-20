4d846ccf736b53313bb2347479c5e95f05ab1579
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-20 18:17:15 +0300

    .

--- desktop/lib/widgets/bottom_tab.dart
+++ desktop/lib/widgets/bottom_tab.dart
@@ -1,5 +1,7 @@
 import 'package:desktop/config/style.dart';
+import 'package:desktop/pages/auth/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BottomTab extends StatefulWidget {
   const BottomTab({Key? key}) : super(key: key);
@@ -10,6 +12,13 @@ class BottomTab extends StatefulWidget {
 
 class _BottomTabState extends State<BottomTab> {
   bool scannerConnected=false;
+//  getData() async{
+//     final pref = await SharedPreferences.getInstance();
+
+//     String? username =  pref.getString('username');
+   
+//   return username;
+//   }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,46 +26,53 @@ class _BottomTabState extends State<BottomTab> {
       width: double.infinity,
       color: Colors.blue,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          const Icon(
-            Icons.person,
-            size: 40,
-            color: Colors.white,
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 0, top: 20, bottom: 20),
-            child: Row(
-              children: [
-                Text(
-                  "Logged in as:",
-                  style: bottomTabLeading,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0, right: 10),
-                  child: Text(
-                    "Admin",
-                    style: bottomTabUserInfo,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 10.0),
-                  child: Text(
-                    "Email:",
-                    style: bottomTabLeading,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Text(
-                    "johnmungai392@gmail.com",
-                    style: bottomTabUserInfo,
-                  ),
-                ),
-              ],
-            ),
-          ),
+          // const Icon(
+          //   Icons.person,
+          //   size: 40,
+          //   color: Colors.white,
+          // ),
+          // Container(
+          //   margin: const EdgeInsets.only(left: 0, top: 20, bottom: 20),
+          //   child: Row(
+          //     children: [
+          //       // Text(
+          //       //   "Logged in as:",
+          //       //   style: bottomTabLeading,
+          //       // ),
+          //       // Padding(
+          //       //   padding: const EdgeInsets.only(left: 10.0, right: 10),
+          //       //   child: Text(
+                    
+          //       //   '',
+          //       //     style: bottomTabUserInfo,
+          //       //   ),
+          //       // ),
+          //       // Padding(
+          //       //   padding: const EdgeInsets.only(right: 10.0),
+          //       //   child: Text(
+          //       //     "Email:",
+          //       //     style: bottomTabLeading,
+          //       //   ),
+          //       // ),
+          //       // Padding(
+          //       //   padding: const EdgeInsets.only(right: 8.0),
+          //       //   child: Text(
+          //       //     "johnmungai392@gmail.com",
+          //       //     style: bottomTabUserInfo,
+          //       //   ),
+          //       // ),
+          //     ],
+          //   ),
+          // ),
           TextButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Auth()));
+            },
             child: const Text(
               'Logout',
               style:
