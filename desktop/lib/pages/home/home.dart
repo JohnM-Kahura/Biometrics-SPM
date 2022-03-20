2b583df215d8ac198565405d36193ddcacd31468
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-20 18:17:07 +0300

    post data ui

--- desktop/lib/pages/home/home.dart
+++ desktop/lib/pages/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:desktop/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,6 +14,8 @@ class _HomeState extends State<Home> {
   TextEditingController middlenameController = TextEditingController();
   TextEditingController lastnameController = TextEditingController();
   TextEditingController studentIDController = TextEditingController();
+  bool fieldsMissing = false;
+  bool badRequest = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,17 +31,69 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               customTextFeild(50, double.infinity, 'First name',
-                  firstnameController, TextInputType.name,false),
+                  firstnameController, TextInputType.name, false),
               customTextFeild(50, double.infinity, 'Middle name',
-                  middlenameController, TextInputType.name,false),
+                  middlenameController, TextInputType.name, false),
               customTextFeild(50, double.infinity, 'Last name',
-                  lastnameController, TextInputType.name,false),
+                  lastnameController, TextInputType.name, false),
               customTextFeild(50, double.infinity, 'StudentID',
-                  studentIDController, TextInputType.name,false),
+                  studentIDController, TextInputType.name, false),
+              if (fieldsMissing)
+                const Text(
+                  'Some Required info is missing',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              if (badRequest)
+                const Text(
+                  'Bad Request check the data being posted',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               Container(
                 margin: const EdgeInsets.only(top: 30),
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    var url = Uri.parse('http://127.0.0.1:8000/api/');
+
+                    if (firstnameController.text.isNotEmpty &&
+                        middlenameController.text.isNotEmpty &&
+                        lastnameController.text.isNotEmpty &&
+                        studentIDController.text.isNotEmpty) {
+                      var response = await http.post(url, body: {
+                        'firstname': firstnameController.text,
+                        'middlename': middlenameController.text,
+                        'lastname': lastnameController.text,
+                        'studentID': studentIDController.text,
+                        //TODO: send the fingerprint object here later
+                      });
+                      if (response.statusCode == 201) {
+                        // ignore: deprecated_member_use
+                        Scaffold.of(context).showSnackBar(const SnackBar(
+                          content: Text('User Registration Successfull'),
+                          duration: Duration(seconds: 1),
+                          backgroundColor: Colors.green,
+                        ));
+                      }
+                      if (response.statusCode != 201) {
+                        setState(() {
+                          badRequest = true;
+                        });
+                      }
+                    }
+                    if (firstnameController.text.isEmpty ||
+                        middlenameController.text.isEmpty ||
+                        lastnameController.text.isEmpty ||
+                        studentIDController.text.isEmpty) {
+                      setState(() {
+                        fieldsMissing = true;
+                      });
+                    }
+                  },
                   child: const Text(
                     'Register',
                     style: TextStyle(
@@ -77,7 +132,7 @@ class _HomeState extends State<Home> {
                   child: Column(
                     children: [
                       Row(
-                        children: const  [ Text('Status:'), Text('Sucess')],
+                        children: const [Text('Status:'), Text('Sucess')],
                       ),
                       Row(
                         children: const [
